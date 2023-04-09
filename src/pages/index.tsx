f6c64dd600a054289d73e7fd0ca939837a09a82b
Barbosa885 <gustavosports7@gmail.com>
2023-04-09 12:00:55 -0300

    WIP

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <>
-      <span className="">teste</span>
-    </>
+    <div className="bg-gray-500 h-screen w-full justify-center items-center">
+      <span className="text-white text-lg text-bold">teste</span>
+    </div>
   );
 }
